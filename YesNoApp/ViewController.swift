e13fb162a6668b7189b9dbb2125a7c690e0e4d99
olgazinchenko <ozinchenko.pm@gmail.com>
2024-02-21 16:33:08 +0100

    Initial Commit

---
+++ YesNoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesNoApp
+//
+//  Created by ozinchenko.dev on 21/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
